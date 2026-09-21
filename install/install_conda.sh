88bc36871a9978d9f59aa21970e4aeb8b9ec5992
chengzhongshan <cheng.zhong.shan@gmail.com>
2026-09-20 20:11:05 -0500

    Fix Conda and Cygwin installation jobs

--- install/install_conda.sh
+++ install/install_conda.sh
@@ -7,7 +7,8 @@ command_exists conda || die "conda not found. Install Miniconda/Anaconda first."
 
 ENV_NAME="${MCP4SAS_CONDA_ENV:-mcp4sas}"
 log "Creating/updating conda environment ${ENV_NAME}"
-conda create -y -n "${ENV_NAME}" -c conda-forge python=3.11 perl openjdk curl make compilers cpanminus
+conda create -y -n "${ENV_NAME}" -c conda-forge \
+  python=3.11 perl openjdk curl make compilers perl-app-cpanminus
 
 CONDA_PYTHON="$(conda run -n "${ENV_NAME}" python - <<'PY'
 import sys
